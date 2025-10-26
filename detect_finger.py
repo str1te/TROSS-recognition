71f8178188496ee987ae8846826e9475dbbe73f7
str1te <zan.alexplay@gmail.com>
2025-10-26 16:17:03 +0300

    fix/26102025/fix_shortcomings

--- detect_finger.py
+++ detect_finger.py
@@ -3,59 +3,121 @@ import numpy as np
 import mediapipe as mp
 
 
-class detect_fingers():
-    def __init__(self):
+class DetectingFingers():
+    def __init__(self, index: int = None):
         print("Initializing the camera...")
-        camera_found = False
-        for camera_index in [0, 1, 2]:
-            for backend in [cv2.CAP_ANY, cv2.CAP_DSHOW]:
-                print(f"Trying the camera {camera_index} with a backend {backend}")
-                self.cap = cv2.VideoCapture(camera_index, backend)
-                if self.cap.isOpened():
-                    ret, test_frame = self.cap.read()
-                    if ret and test_frame is not None:
-                        print(f"Successful: Camera {camera_index}, backend {backend}")
-                        camera_found = True
+        
+        def find_camera():
+                camera_test = False   
+                camera_index = 0  
+                while True:
+                    camera_found = False 
+                    for backend in [cv2.CAP_ANY, cv2.CAP_DSHOW]:
+                        cap = cv2.VideoCapture(camera_index, backend)
+                        if cap.isOpened():
+                            camera_found = True
+                            camera_test = True
+                        else:
+                            cap.release()
+                            break
+                    if camera_found:
+                        camera_index += 1
+                        
+                    if not camera_found:
+                            break
+                
+                    if not camera_test:
+                        return 
+
+                    return list(range(camera_index))
+        
+        available_cameras = find_camera()
+    
+        if index is not None:
+            self.cap = cv2.VideoCapture(index)
+            self.camera_test = True
+            if not self.cap.isOpened():
+                print(f"Error:Camera with index {index} dont working")
+                self.camera_test = False
+                while True:    
+                    answer = str(input('Do you want to identify the available cameras? Y/N\n'))
+                    if answer in ('y', 'Y'):
+                        answer = 1
                         break
                     else:
-                        self.cap.release()
+                        if answer in ('n', 'N'):
+                            self.camera_test = False
+                            answer = 0
+                            return
+                        else:
+                            print('Error:Enter Y or N')
+
+                if answer:                                              
+                    if available_cameras is None:
+                        print("Couldn't find a working camera")
+                        return
+                    else:
+                        while True:    
+                            try:
+                                print(f"list of working camera indexes: {','.join(map(str,available_cameras))}")
+                                index = int(input("Plese, select a camera from the list of found indexes\n"))
+                                if index in available_cameras:
+                                    self.cap = cv2.VideoCapture(index)
+                                    self.camera_test = True
+                                    break
+                                else:
+                                    print("Error: The selected index is not in the list. Please try again.")
+                            except ValueError:
+                                print("Error: Please enter a valid integer.")
                 else:
-                    self.cap.release()
-            if camera_found:
-                break
-        
-        if not camera_found:
-            print("Couldn't find a working camera")
-            self.cap = None
-            return       
-    
-        self.mpHand = mp.solutions.hands
-        self.hands = self.mpHand.Hands()
-        self.mpDraw = mp.solutions.drawing_utils
+                    return
+
+        else:
+            if available_cameras is None:
+                print("Couldn't find a working camera")
+                return
+            else:
+                while True:    
+                    try:
+                        print(f"list of working camera indexes: {','.join(map(str,available_cameras))}")
+                        index = int(input("Plese, select a camera from the list of found indexes\n"))
+                        if index in available_cameras:
+                            self.cap = cv2.VideoCapture(index)
+                            self.camera_test = True
+                            break
+                        else:
+                            print("Error: The selected index is not in the list. Please try again.")
+                    except ValueError:
+                        print("Error: Please enter a valid integer.")
+ 
+        self.mp_hand = mp.solutions.hands
+        self.hands = self.mp_hand.Hands()
+        self.mp_draw = mp.solutions.drawing_utils
     
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         self.cap.set(cv2.CAP_PROP_FPS, 30)
 
-        print("Initialization is completed")
+        print("Initialization is completed!")
     
-    
-    def count_fingers(self, hand_landmarks):
+    def count_fingers(self, hand_landmarks) -> any:
         finger_tips = [4, 8, 12, 16, 20]
         finger_pips = [3, 6, 10, 14, 18]
 
         fingers_up = []
 
         thumb_tip = hand_landmarks.landmark[4]
-        thumb_mcp = hand_landmarks.landmark[2]
+        base_littlefinger = hand_landmarks.landmark[17]
+        tip_middlefinger = hand_landmarks.landmark[2]
+        base_hand = hand_landmarks.landmark[0]
 
-        if hand_landmarks.landmark[12].y > hand_landmarks.landmark[0].y:
-            if thumb_tip.y >= hand_landmarks.landmark[17].y:
+        if tip_middlefinger.y > base_hand.y:
+            if thumb_tip.y >= base_littlefinger.y:
                 fingers_up.append(1)
             else:
                 fingers_up.append(0)
 
-            for i in range(1,5):
+            for i in range(1, 5):
                 tip = hand_landmarks.landmark[finger_tips[i]]
                 pip = hand_landmarks.landmark[finger_pips[i]]
 
@@ -64,12 +126,12 @@ class detect_fingers():
                 else:
                     fingers_up.append(0)
         else:
-            if thumb_tip.y <= hand_landmarks.landmark[17].y:
+            if thumb_tip.y <= base_littlefinger.y:
                 fingers_up.append(1)
             else:
                 fingers_up.append(0)
 
-            for i in range(1,5):
+            for i in range(1, 5):
                 tip = hand_landmarks.landmark[finger_tips[i]]
                 pip = hand_landmarks.landmark[finger_pips[i]]
 
@@ -82,32 +144,38 @@ class detect_fingers():
 
     
     def detecting(self):
+        if not self.camera_test:
+            return
+        
         while True:
-            r, frame = self.cap.read()
-            if not r:
+            accept_img, frame = self.cap.read()
+            if not accept_img:
                 print("Error: couldn't get a frame")
                 break
             
-            frame = cv2.flip(frame,1)
-            cvt_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            mirrored_frame = cv2.flip(frame, 1)
+            cvt_frame = cv2.cvtColor(mirrored_frame, cv2.COLOR_BGR2RGB)
             results = self.hands.process(cvt_frame)
             
             if results.multi_hand_landmarks:
                 for hand_idx, handLms in enumerate(results.multi_hand_landmarks):
                     finger_count, fingers_status = self.count_fingers(handLms)
-                    self.mpDraw.draw_landmarks(frame, handLms, self.mpHand.HAND_CONNECTIONS)
+                    self.mp_draw.draw_landmarks(mirrored_frame, handLms, self.mp_hand.HAND_CONNECTIONS)
                     info_y = 50 + hand_idx * 100
                     
                     cv2.putText(
-                                frame, f'Hand {hand_idx + 1}: {finger_count} fingers', 
-                                (10, info_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2
-                                )
+                                mirrored_frame, f'Hand {hand_idx + 1}: {finger_count} fingers', 
+                                (10, info_y), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 
+                                1, (255,255,255), 2
+                    )
             
-            cv2.imshow('Camera',frame)
+            cv2.imshow('Camera', mirrored_frame)
             key = cv2.waitKey(1) & 0xFF  
             
             if key == ord('q'):
                 print("Exit by key Q") 
                 break
+        
         self.cap.release()
         cv2.destroyAllWindows()   
